d8a044ddeb3c2caccb68e917a79ad14e3987ddd3
junewong <junewong12@gmail.com>
2016-11-19 08:01:39 -0800

    urlcrawl.sh

--- urlcrawl.sh
+++ urlcrawl.sh
@@ -2,4 +2,7 @@
 
 url=$1
 today=`date +"%Y-%m-%d"`
-crawler "$1" ".feed_item h2" |grep $today |html2text |sed "s/ *分享$//g" |sed -e "s/^## *\[ \([^\[]\+\) \](\([^)]\+\)) \+\([^ ]\+\)$/\2^\1^\3/g"  |sed -e "/^ *$/d" -e "s#^#http://chuansong.me#g"
+crawler "$1" ".feed_item h2" |grep $today |html2text -utf8\
+	|sed "s/ *分享$//g" \
+	|sed -e "s/^## *\[ \([^\[]\+\) \](\([^)]\+\)) \+\([^ ]\+\)$/\2^\1^\3/g"  \
+	|sed -e "/^ *$/d" -e "s#^#http://chuansong.me#g"
