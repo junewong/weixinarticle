b4df0847cbe6e764cdc01cad4d0491353fa37bd8
junewong <junewong12@gmail.com>
2016-11-20 01:53:19 +0800

    update weixin articles

--- run.sh
+++ run.sh
@@ -1,10 +1,24 @@
+source ~/.wxrc
+
+log (){
+	today=`date +"%Y-%m-%d"`
+	time=`date +"%Y-%m-%d %H:%M:%S"`
+	echo "[$time] $*" |tee -a logs/${today}.log
+}
+
 today=`date +"%Y-%m-%d"`
 
 file="htmls/${today}.html"
 index="htmls/index.html"
 
-echo "Starting ..."
+log "Starting ..."
+git pull
 ./multicrawl.sh
 ./convert2html.sh origin_contet.txt > $file
 cp $file $index
-echo "Done"
+log "Done"
+
+log "try to callback $WX_CAllBACk ..."
+if [[ -e "$WX_CAllBACk" ]];then
+	sh $WX_CAllBACk
+fi
