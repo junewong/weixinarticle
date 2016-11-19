9282886daee483ea68f972ee88420a1f18830ab9
junewong <junewong12@gmail.com>
2016-11-19 09:11:50 -0800

    modify

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 today=`date +"%Y-%m-%d"`
 
 file="htmls/${today}.html"
+index="htmls/index.html"
 
 echo "Starting ..."
 ./multicrawl.sh
 ./convert2html.sh origin_contet.txt > $file
-cp $file index.html
+cp $file $index
 echo "Done"
