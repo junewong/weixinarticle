41b22304ccf30fc486dc32acad5efd51e10c87c1
junewong <junewong12@gmail.com>
2016-11-20 02:03:05 +0800

    modify

--- run.sh
+++ run.sh
@@ -1,2 +1,24 @@
-source ~/.wxrc
-sh "$WX_CAllBACk"
+source /home/pi/.wxrc
+
+log (){
+	today=`date +"%Y-%m-%d"`
+	time=`date +"%Y-%m-%d %H:%M:%S"`
+	echo "[$time] $*" |tee -a logs/${today}.log
+}
+
+today=`date +"%Y-%m-%d"`
+
+file="htmls/${today}.html"
+index="htmls/index.html"
+
+log "Starting ..."
+git pull
+./multicrawl.sh
+./convert2html.sh origin_contet.txt > $file
+cp $file $index
+log "Done"
+
+log "try to callback $WX_CAllBACk ..."
+if [[ -e "$WX_CAllBACk" ]];then
+	sh $WX_CAllBACk
+fi
