028f07e8ad5414523a819768a5bc66dc3a4e4829
junewong <junewong12@gmail.com>
2016-11-21 13:42:08 +0800

    sorted random

--- multicrawl.sh
+++ multicrawl.sh
@@ -4,13 +4,13 @@ date=$1
 
 file="tmp_data.txt"
 
-cat feed.txt | sed -e "/^ *$/d" -e "s/ \+/ /g" |while read line
+cat feed.txt |sort -R | sed -e "/^ *$/d" -e "s/ \+/ /g" |while read line
 do 
 	url=`echo $line |cut -d" " -f1`
 	author=`echo $line |cut -d" " -f2`
 	>&2 echo $url $author
 	./urlcrawl.sh $url $author $date
-	rand=`awk -v min=2 -v max=7 'BEGIN{srand(); print int(min+rand()*(max-min+1))}'`
+	rand=`awk -v min=5 -v max=12 'BEGIN{srand(); print int(min+rand()*(max-min+1))}'`
 	>&2 echo "sleep $rand ..."
 	sleep $rand
 
