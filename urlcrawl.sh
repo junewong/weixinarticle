3bb1c31f510a8e94bd4125ff92a1d8e5a2f67618
junewong <junewong12@gmail.com>
2016-12-09 00:41:36 +0800

    remove proxy

--- urlcrawl.sh
+++ urlcrawl.sh
@@ -7,7 +7,10 @@ if [[ "$date" = "" ]];then
 	date=`date +"%Y-%m-%d"`
 fi
 
-crawler "$url" "a.question_link, .timestamp"  \
+#proxy="-x http://211.153.17.151:80"
+proxy=""
+
+curl --connect-timeout 5 $proxy "$url" |crawler "a.question_link, .timestamp"  \
 	| tr -d "\n"  \
 	| sed -e"s/<\/span>/\n/g" -e 's/href="/>/g' -e 's/" target/</g' -e "s/<[^>]\+>/^/g"  \
 	| sed -e "s/^ *\^//" -e "s/\^\^/^/g" \
